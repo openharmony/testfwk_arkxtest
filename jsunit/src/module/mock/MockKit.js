20bcb9cf905e0da6e9821ca16ef48de863105f4e
yuanchao <yuanchao52@h-partners.com>
2022-07-06 14:26:38 +0800

    hypium Signed-off-by: <wanglongjin1@huawei.com>
    Signed-off-by: yuanchao <yuanchao52@h-partners.com>

--- jsunit/src/module/mock/MockKit.js
+++ jsunit/src/module/mock/MockKit.js
@@ -13,7 +13,7 @@
  * limitations under the License.
  */
 
-import ExtendInterface from "@ohos/hypium/src/module/mock/ExtendInterface";
+import ExtendInterface from "./ExtendInterface";
 import {
     ArgumentMatchers,
     any,
@@ -25,7 +25,7 @@ import {
     matcheReturnKey,
     matcheStubKey,
     isRegExp,
-} from "@ohos/hypium/src/module/mock/ArgumentMatchers";
+} from "./ArgumentMatchers";
 
 class MockKit {
     constructor() {
