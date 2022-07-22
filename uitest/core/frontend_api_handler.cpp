9b8df9bf0b006ee430f6543988583393c2b254f4
zhouke <zhouke35@huawei.com>
2022-07-22 14:06:55 +0800

    修复scrollSearch找不到控件.Signed-off-by: <zhouke35@huawei.com>.
    Signed-off-by: zhouke <zhouke35@huawei.com>

--- uitest/core/frontend_api_handler.cpp
+++ uitest/core/frontend_api_handler.cpp
@@ -615,7 +615,13 @@ namespace OHOS::uitest {
                 wOp.InputText("", out.exception_);
             } else if (in.apiId_ == "UiComponent.scrollSearch") {
                 auto &selector = GetBackendObject<WidgetSelector>(ReadCallArg<string>(in, INDEX_ZERO));
-                wOp.ScrollFindWidget(selector, out.exception_);
+                auto result = wOp.ScrollFindWidget(selector, out.exception_);
+                if (result != nullptr) {
+                    const auto driverRef = sDriverBindingMap.find(in.callerObjRef_)->second;
+                    out.resultValue_ = StoreBackendObject(move(result), driverRef);
+                } else {
+                    out.resultValue_ = nullptr;
+                }
             } else if (in.apiId_ == "UiComponent.pinchOut") {
                 auto pinchScale = ReadCallArg<float_t>(in, INDEX_ZERO);
                 wOp.PinchWidget(pinchScale, out.exception_);
