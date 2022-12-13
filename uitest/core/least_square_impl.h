d8a09e4557397f3a9f4f5e7b21f262824979f008
chen828 <chensihui6@huawei.com>
2022-12-13 11:23:38 +0800

    codecheck
    Signed-off-by: chen828 <chensihui6@huawei.com>

--- uitest/core/least_square_impl.h
+++ uitest/core/least_square_impl.h
@@ -27,22 +27,22 @@ namespace OHOS::uitest {
  * the function template is a3 * x^3 + a2 * x^2 + a1 * x + a0 = y with four;
  * the function template is 0 * x^3 + a2 * x^2 + a1 * x + a0 = y with three.
  */
-class LSMImpl {
+class LeastSquareImpl {
 public:
     /**
      * @brief Construct a new Least Square Impl object.
      * @param paramsNum the right number is 4 or 3.
      */
-    explicit LSMImpl(int32_t paramsNum) : paramsNum_(paramsNum) {}
+    explicit LeastSquareImpl(int32_t paramsNum) : paramsNum_(paramsNum) {}
 
     /**
      * @brief Construct a new Least Square Impl object.
      * @param paramsNum the right number is 4 or 3.
      */
-    LSMImpl(int32_t paramsNum, int32_t countNum) : paramsNum_(paramsNum), countNum_(countNum) {}
+    LeastSquareImpl(int32_t paramsNum, int32_t countNum) : paramsNum_(paramsNum), countNum_(countNum) {}
 
-    LSMImpl() = default;
-    ~LSMImpl() = default;
+    LeastSquareImpl() = default;
+    ~LeastSquareImpl() = default;
 
     void UpdatePoint(double tVal, double pVal)
     {
