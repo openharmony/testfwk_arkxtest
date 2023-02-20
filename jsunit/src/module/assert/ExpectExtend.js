23b3ae54d76b2ba2e9cd63185e331915a18e511e
lijialang <lijialang2@huawei.com>
2023-02-20 19:40:17 +0800

    删除提交
    Signed-off-by: lijialang <lijialang2@huawei.com>

--- jsunit/src/module/assert/ExpectExtend.js
+++ jsunit/src/module/assert/ExpectExtend.js
@@ -23,8 +23,6 @@ import assertUndefined from './assertUndefined';
 import assertFalse from './assertFalse';
 import assertInstanceOf from './assertInstanceOf';
 import assertThrowError from './assertThrowError';
-import assertLargerOrEqual from './assertLargerOrEqual'
-import assertLessOrEqual from './assertLessOrEqual'
 import assertNaN from './assertNaN'
 import assertNegUnlimited from './assertNegUnlimited'
 import assertPosUnlimited from './assertPosUnlimited'
@@ -52,8 +50,6 @@ class ExpectExtend {
         this.matchers.assertFalse = assertFalse;
         this.matchers.assertInstanceOf = assertInstanceOf;
         this.matchers.assertThrowError = assertThrowError;
-        this.matchers.assertLargerOrEqual = assertLargerOrEqual;
-        this.matchers.assertLessOrEqual = assertLessOrEqual;
         this.matchers.assertNaN = assertNaN;
         this.matchers.assertNegUnlimited = assertNegUnlimited;
         this.matchers.assertPosUnlimited = assertPosUnlimited;
