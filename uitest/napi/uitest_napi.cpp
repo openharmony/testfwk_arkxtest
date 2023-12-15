5feb70c259dfcde85fa7e190fd95beaf768c85a8
zhouke <zhouke35@huawei.com>
2023-12-15 15:09:24 +0800

    modify.Signed-off-by: <zhouke35@huawei.com>.
    Signed-off-by: zhouke <zhouke35@huawei.com>

--- uitest/napi/uitest_napi.cpp
+++ uitest/napi/uitest_napi.cpp
@@ -19,9 +19,9 @@
 #include <queue>
 #include <set>
 #include <string>
-#include <fcntl.h>
 #include <unistd.h>
 #include "json.hpp"
+#include "pasteboard_client.h"
 #include "common_utilities_hpp.h"
 #include "frontend_api_defines.h"
 #include "ipc_transactor.h"
@@ -333,11 +333,25 @@ namespace OHOS::uitest {
         return napi_ok;
     }
 
+    static void SetPasteBoardData(string_view text)
+    {
+        auto pasteBoardMgr = MiscServices::PasteboardClient::GetInstance();
+        pasteBoardMgr->Clear();
+        auto pasteData = MiscServices::PasteboardClient::GetInstance()->CreatePlainTextData(string(text));
+        pasteBoardMgr->SetPasteData(*pasteData);
+    }
+
     static void PreprocessTransaction(napi_env env, TransactionContext &ctx, napi_value &error)
     {
         auto &paramList = ctx.callInfo_.paramList_;
         const auto &id = ctx.callInfo_.apiId_;
-        if (id  == "Driver.screenCap" || id  == "UiDriver.screenCap" || id  == "Driver.screenCapture") {
+        if (id  == "Component.inputText" && paramList.size() > 0) {
+            auto text = paramList.at(INDEX_ZERO).get<string>();
+            SetPasteBoardData(text);
+        } else if (id  == "Driver.inputText" && paramList.size() > 0) {
+            auto text = paramList.at(INDEX_ONE).get<string>();
+            SetPasteBoardData(text);
+        } else if (id  == "Driver.screenCap" || id  == "UiDriver.screenCap" || id  == "Driver.screenCapture") {
             if (paramList.size() < 1 || paramList.at(0).type() != nlohmann::detail::value_t::string) {
                 LOG_E("Missing file path argument");
                 error = CreateJsException(env, ERR_INVALID_INPUT, "Missing file path argument");
