2ae2d3ee9565e41da85da5743e87ad62899edf0c
stev <wusipeng1@h-partners.com>
2024-03-12 17:30:50 +0800

    新增api
    Signed-off-by: stev <wusipeng1@h-partners.com>

--- jsunit/src/event.js
+++ jsunit/src/event.js
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2021-2022 Huawei Device Co., Ltd.
+ * Copyright (c) 2021-2024 Huawei Device Co., Ltd.
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -87,6 +87,12 @@ class TaskEvent {
     incorrectFormat() {
         for (const monitor of this.eventMonitors) {
             monitor['incorrectFormat']();
+        }
+    }
+
+    incorrectTestSuiteFormat() {
+        for (const monitor of this.eventMonitors) {
+            monitor['incorrectTestSuiteFormat']();
         }
     }
 }
