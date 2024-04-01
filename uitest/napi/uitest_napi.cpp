1256bf1e52dfd40101a0539f2e53ab154edcdb2a
zhouke <zhouke35@huawei.com>
2024-04-01 10:53:34 +0800

    modify.Signed-off-by: <zhouke35@huawei.com>.
    Signed-off-by: zhouke <zhouke35@huawei.com>

--- uitest/napi/uitest_napi.cpp
+++ uitest/napi/uitest_napi.cpp
@@ -349,10 +349,15 @@ namespace OHOS::uitest {
         const auto &id = ctx.callInfo_.apiId_;
         if (id  == "Component.inputText" && paramList.size() > 0) {
             auto text = paramList.at(INDEX_ZERO).get<string>();
-            SetPasteBoardData(text);
-        } else if (id  == "Driver.inputText" && paramList.size() > 0) {
-            auto text = paramList.at(INDEX_ONE).get<string>();
-            SetPasteBoardData(text);
+            if (paramList.at(INDEX_ZERO) != nlohmann::detail::value_t::null) {
+                auto text0 = paramList.at(INDEX_ZERO).get<string>();
+                SetPasteBoardData(text0);
+            }
+        } else if (id  == "Driver.inputText" && paramList.size() > 1) {
+            if (paramList.at(INDEX_ONE) != nlohmann::detail::value_t::null) {
+                auto text1 = paramList.at(INDEX_ONE).get<string>();
+                SetPasteBoardData(text1);
+            }
         } else if (id  == "Driver.screenCap" || id  == "UiDriver.screenCap" || id  == "Driver.screenCapture") {
             if (paramList.size() < 1 || paramList.at(0).type() != nlohmann::detail::value_t::string) {
                 LOG_E("Missing file path argument");
