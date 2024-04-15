d76927602ade078065efab5cce01b809db3bbc48
Catherine_MK <houjing8@huawei.com>
2024-04-15 17:01:17 +0800

    简化测试目录，runner作为执行器启动测试框架
    houjing8@huawei.com
    
    Signed-off-by: Catherine_MK <houjing8@huawei.com>

--- jsunit/src/testrunner/OpenHarmonyTestRunner.ts
+++ jsunit/src/testrunner/OpenHarmonyTestRunner.ts
@@ -24,28 +24,29 @@ import testsuite from '../test/List.test';
 let abilityDelegator: abilityDelegatorRegistry.AbilityDelegator;
 let abilityDelegatorArguments: abilityDelegatorRegistry.AbilityDelegatorArgs;
 let jsonPath: string = 'mock/mock-config.json';
-let tag: string = 'testTag';
+let domain: number = 0x0000; //日志标识,0x0000作为测试框架的业务标识
+let tag: string = 'testTag'; //日志标识字符串,作为tag标识当前runner类下的测试行为
 
 export default class OpenHarmonyTestRunner implements TestRunner {
   constructor() {
   }
 
   onPrepare() {
-    hilog.info(0x0000, 'testTag', '%{public}s', 'OpenHarmonyTestRunner OnPrepare');
+    hilog.info(domain, 'testTag', '%{public}s', 'OpenHarmonyTestRunner OnPrepare');
   }
 
   async onRun() {
     let tag = 'testTag';
-    hilog.info(0x0000, tag, '%{public}s', 'OpenHarmonyTestRunner onRun run');
+    hilog.info(domain, tag, '%{public}s', 'OpenHarmonyTestRunner onRun run');
     abilityDelegatorArguments = abilityDelegatorRegistry.getArguments();
     abilityDelegator = abilityDelegatorRegistry.getAbilityDelegator();
     let moduleName = abilityDelegatorArguments.parameters['-m'];
     let context = abilityDelegator.getAppContext().getApplicationContext().createModuleContext(moduleName);
     let mResourceManager = context.resourceManager;
     await checkMock(abilityDelegator, mResourceManager);
-    hilog.info(0x0000, 'testTag', '%{public}s', 'start run testcase!!!');
+    hilog.info(domain, 'testTag', '%{public}s', 'start run testcase!!!');
     Hypium.hypiumTest(abilityDelegator, abilityDelegatorArguments, testsuite);
-    hilog.info(0x0000, tag, '%{public}s', 'OpenHarmonyTestRunner onRun end');
+    hilog.info(domain, tag, '%{public}s', 'OpenHarmonyTestRunner onRun end');
   }
 }
 
@@ -53,7 +54,7 @@ async function checkMock(abilityDelegator: abilityDelegatorRegistry.AbilityDeleg
   let rawFile: Uint8Array;
   try {
     rawFile = resourceManager.getRawFileContentSync(jsonPath);
-    hilog.info(0x0000, tag, 'MockList file exists');
+    hilog.info(domain, tag, 'MockList file exists');
     let mockStr: string = util.TextDecoder.create("utf-8", { ignoreBOM: true }).decodeWithStream(rawFile);
     let mockMap: Record<string, string> = getMockList(mockStr);
     try {
@@ -61,12 +62,12 @@ async function checkMock(abilityDelegator: abilityDelegatorRegistry.AbilityDeleg
     } catch (error) {
       let code = (error as BusinessError).code;
       let message = (error as BusinessError).message;
-      hilog.error(0x0000, tag, `abilityDelegator.setMockList failed, error code: ${code}, message: ${message}.`);
+      hilog.error(domain, tag, `abilityDelegator.setMockList failed, error code: ${code}, message: ${message}.`);
     }
   } catch (error) {
     let code = (error as BusinessError).code;
     let message = (error as BusinessError).message;
-    hilog.error(0x0000, tag, `ResourceManager:callback getRawFileContent failed, error code: ${code}, message: ${message}.`);
+    hilog.error(domain, tag, `ResourceManager:callback getRawFileContent failed, error code: ${code}, message: ${message}.`);
   }
 }
 
@@ -78,6 +79,6 @@ function getMockList(jsonStr: string) {
     let realValue: string = value['source'].toString();
     mockList[key] = realValue;
   });
-  hilog.info(0x0000, tag, '%{public}s', 'mock-json value:' + JSON.stringify(mockList) ?? '');
+  hilog.info(domain, tag, '%{public}s', 'mock-json value:' + JSON.stringify(mockList) ?? '');
   return mockList;
 }
