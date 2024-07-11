fa157ba73735de75d4f98807168ab996d6c6ffdc
lijialang <lijialang2@huawei.com>
2024-07-11 11:46:38 +0800

    代码告警处理
    Signed-off-by: lijialang <lijialang2@huawei.com>

--- jsunit/src/module/report/LogExpectError.js
+++ jsunit/src/module/report/LogExpectError.js
@@ -15,56 +15,51 @@
 
 class LogExpectError {
     static getErrorMsg(matcherName, actualValue, expect, originMsg) {
-        if (matcherName === "assertNull") {
-            return "expect not null, actualValue is " + (actualValue)
+        if (matcherName === 'assertNull') {
+            return 'expect not null, actualValue is ' + (actualValue);
         }
-        if (matcherName === "assertTrue") {
-            return "expect not true, actualValue is " + (actualValue)
+        if (matcherName === 'assertTrue') {
+            return 'expect not true, actualValue is ' + (actualValue);
         }
-        if (matcherName === "assertFalse") {
-            return "expect not false, actualValue is " + (actualValue)
+        if (matcherName === 'assertFalse') {
+            return 'expect not false, actualValue is ' + (actualValue);
         }
-        if (matcherName === "assertEqual") {
-            return "expect not Equal, actualValue is "
-                + actualValue + ' equals ' + expect;
+        if (matcherName === 'assertEqual') {
+            return 'expect not Equal, actualValue is ' + actualValue + ' equals ' + expect;
         }
-        if (matcherName === "assertContain") {
-            return "expect not have, " + actualValue + " have " + expect;
+        if (matcherName === 'assertContain') {
+            return 'expect not have, ' + actualValue + ' have ' + expect;
         }
-        if (matcherName === "assertInstanceOf") {
-            return "expect not InstanceOf, "
-                + actualValue + ' is '
-                + Object.prototype.toString.call(actualValue) +  expect;
+        if (matcherName === 'assertInstanceOf') {
+            return 'expect not InstanceOf, ' + actualValue + ' is ' +
+            Object.prototype.toString.call(actualValue) + expect;
         }
-        if (matcherName === "assertLarger") {
-            return "expect not Larger, "
-                + (actualValue) + ' is larger than ' + expect;
+        if (matcherName === 'assertLarger') {
+            return 'expect not Larger, ' +
+                (actualValue) + ' is larger than ' + expect;
         }
-        if (matcherName === "assertLargerOrEqual") {
-            return "expect not LargerOrEqual, "
-                + (actualValue) + ' larger than ' + expect;
+        if (matcherName === 'assertLargerOrEqual') {
+            return 'expect not LargerOrEqual, ' + (actualValue) + ' larger than ' + expect;
         }
-        if (matcherName === "assertLess") {
-            return "expect not Less, "
-                + (actualValue) + ' less than ' + expect;
+        if (matcherName === 'assertLess') {
+            return 'expect not Less, ' + (actualValue) + ' less than ' + expect;
         }
-        if (matcherName === "assertLessOrEqual") {
-            return "expect not LessOrEqual, "
-                + (actualValue) + ' is less than ' + expect;
+        if (matcherName === 'assertLessOrEqual') {
+            return 'expect not LessOrEqual, ' + (actualValue) + ' is less than ' + expect;
         }
-        if (matcherName === "assertNaN") {
-            return "expect not NaN, actualValue is " + (actualValue)
+        if (matcherName === 'assertNaN') {
+            return 'expect not NaN, actualValue is ' + (actualValue);
         }
-        if (matcherName === "assertNegUnlimited") {
-            return "expect not NegUnlimited, actualValue is " + (actualValue)
+        if (matcherName === 'assertNegUnlimited') {
+            return 'expect not NegUnlimited, actualValue is ' + (actualValue);
         }
-        if (matcherName === "assertPosUnlimited") {
-            return "expect not PosUnlimited, actualValue is " + (actualValue)
+        if (matcherName === 'assertPosUnlimited') {
+            return 'expect not PosUnlimited, actualValue is ' + (actualValue);
         }
-        if (matcherName === "assertUndefined") {
-            return "expect not Undefined, actualValue is " + (actualValue)
+        if (matcherName === 'assertUndefined') {
+            return 'expect not Undefined, actualValue is ' + (actualValue);
         }
         return originMsg;
     }
 }
-export default  LogExpectError
+export default LogExpectError;
