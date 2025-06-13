08b97956778f579cfa4d42694489b69525295389
zhouke <zhouke35@huawei.com>
2025-06-13 15:41:59 +0800

    uitest 通过inner-api实现窗口模式切换.Signed-off-by: <zhouke35@huawei.com>.
    Signed-off-by: zhouke <zhouke35@huawei.com>

--- testserver/src/service/test_server_service.cpp
+++ testserver/src/service/test_server_service.cpp
@@ -378,7 +378,7 @@ namespace OHOS::testserver {
         HiLog::Info(LABEL_SERVICE, "Begin to updateWindowLayoutById %{public}d, mode: %{public}d.", windowId, mode);
         auto ret = sceneSessionManager->UpdateWindowLayoutById(windowId, mode);
         HiLog::Info(LABEL_SERVICE, "UpdateWindowLayoutById over, ret: %{public}d", ret);
-        return ret == OHOS::Rosen::WMError_WM_OK ? TEST_SERVER_OK : TEST_SERVER_OPERATE_WINDOW_FAILED;
+        return ret == OHOS::Rosen::WMError::WM_OK ? TEST_SERVER_OK : TEST_SERVER_OPERATE_WINDOW_FAILED;
     }
 
     ErrCode TestServerService::TerminateWindow(int windowId)
@@ -386,9 +386,9 @@ namespace OHOS::testserver {
         HiLog::Info(LABEL_SERVICE, "%{public}s called.", __func__);
         auto sceneSessionManager = Rosen::SessionManagerLite::GetInstance().GetSceneSessionManagerLiteProxy();
         HiLog::Info(LABEL_SERVICE, "Begin to terminateWindow %{public}d", windowId);
-        auto ret = sceneSessionManager->TerminateSessionByPersistenId(windowId);
+        auto ret = sceneSessionManager->TerminateSessionByPersistentId(windowId);
         HiLog::Info(LABEL_SERVICE, "TerminateWindow over, ret: %{public}d", ret);
-        return ret == OHOS::Rosen::WMError_WM_OK ? TEST_SERVER_OK : TEST_SERVER_OPERATE_WINDOW_FAILED;
+        return ret == OHOS::Rosen::WMError::WM_OK ? TEST_SERVER_OK : TEST_SERVER_OPERATE_WINDOW_FAILED;
     }
 
     ErrCode TestServerService::MinimizeWindow(int windowId)
@@ -398,6 +398,6 @@ namespace OHOS::testserver {
         HiLog::Info(LABEL_SERVICE, "Begin to minimizeWindow %{public}d", windowId);
         auto ret = sceneSessionManager->PendingSessionToBackgroundByPersistenId(windowId);
         HiLog::Info(LABEL_SERVICE, "MinimizeWindow over, ret: %{public}d", ret);
-        return ret == OHOS::Rosen::WSError_WM_OK ? TEST_SERVER_OK : TEST_SERVER_OPERATE_WINDOW_FAILED;
+        return ret == OHOS::Rosen::WSError::WM_OK ? TEST_SERVER_OK : TEST_SERVER_OPERATE_WINDOW_FAILED;
     }
 } // namespace OHOS::testserver
