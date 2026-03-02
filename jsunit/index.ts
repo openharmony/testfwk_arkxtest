0e2826cb60e682df266be52772e3764548e3e162
z60111328 <zengminmin7@h-partners.com>
2026-03-02 14:56:24 +0800

    it接口适配新属性tag标签
    Signed-off-by: z60111328 <zengminmin7@h-partners.com>

--- jsunit/index.ts
+++ jsunit/index.ts
@@ -29,4 +29,7 @@ export declare namespace xit {
     function reason(reason: string): any;
 };
 
-export declare function it(testCaseName: string, attribute: TestType | Size | Level, func: Function, timeout?: number, tag?: string): void;
+export declare function it(testCaseName: string, attribute: TestType | Size | Level, func: Function): void;
+
+// export declare function it(testCaseName: string, attribute: TestType | Size | Level, func: Function, timeout?: number,
+//     tag?: string): void;
