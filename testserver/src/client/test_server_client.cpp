e1d5c0521f429d465fb9bd5ed0a8f29b7b01b115
zhou-ke-0929 <zhou-ke-0929@noreply.gitcode.com>
2026-04-13 09:06:19 +0800

    适配多用户场景进行无障碍服务切换.Co-Authored-by: Agent.Signed-off-by: <zhouke35@huawei.com>.
    Signed-off-by: zhou-ke-0929 <zhou-ke-0929@noreply.gitcode.com>

--- testserver/src/client/test_server_client.cpp
+++ testserver/src/client/test_server_client.cpp
@@ -318,7 +318,7 @@ namespace OHOS::testserver {
             HiLog::Error(LABEL, "%{public}s. Get iTestServerInterface FAILED", __func__);
             return TEST_SERVER_GET_INTERFACE_FAILED;
         }
-        int32_t counts = -1;
+        int32_t counts = 1;
         iTestServerInterface_->GetUserCounts(counts);
         return counts;
     }
@@ -332,7 +332,7 @@ namespace OHOS::testserver {
         }
         int32_t userId = -1;
         iTestServerInterface_->GetUserIdByDisplayId(displayId, userId);
-        return displayId;
+        return userId;
     }
 } // namespace OHOS::testserver
 
